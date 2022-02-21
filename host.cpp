9d3337525dc2d47d112d1f23b17aa00c5327460a
Yuanlong Xiao <yuanlongxiao24@gmail.com>
2022-02-21 15:33:02 -0500

    update all the benchmarks

--- host.cpp
+++ host.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv){
 		device = argv[2];
 	}
 	else{
-		path_prefix = "./src/app";
+		path_prefix = "./src/app/digit_reg512";
 		device = "zcu102";
          inst_name = "pfm_top_i/dynamic_region";
 	}
@@ -24,7 +24,7 @@ int main(int argc, char **argv){
 	pr_inst.print_invalid();
 	// pr_inst.print_connect();
 	// pr_inst.print_pragma();
-	pr_inst.print_tile();
+	// pr_inst.print_tile();
 	pr_inst.print_dfx();
 	pr_inst.print_utilization();
 	pr_inst.gen_xdc();
